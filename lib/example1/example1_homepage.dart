ebb1edbc49956a0f976d76ac33c486e7be712331
Eklu Victor Dziedzorm <55409753+Victorspy-web@users.noreply.github.com>
2023-04-03 07:00:52 +0000

    example 1

---
+++ lib/example1/example1_homepage.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+final currentDate = Provider<DateTime>(
+  (ref) => DateTime.now(),
+);
+
+class Example1HomePage extends ConsumerWidget {
+  const Example1HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final date = ref.watch(currentDate);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Example 1"),
+      ),
+      body: Center(
+        child: Text(
+          date.toIso8601String(),
+          style: TextStyle(
+            fontSize: 22,
+          ),
+        ),
+      ),
+    );
+  }
+}
