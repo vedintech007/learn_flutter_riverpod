0e1d4cd1de894f7dac83bd8f146121091cdea834
Eklu Victor Dziedzorm <55409753+Victorspy-web@users.noreply.github.com>
2023-04-11 23:00:30 +0000

    example 4

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:riverpod_tutorials/home_page/ripples_home.dart';
 
 final nameProvider = Provider((ref) => "Hello VED");
 
+final countProvider = StateProvider<int>((ref) => 0);
+
 void main() {
   runApp(const ProviderScope(
     child: RipplesHome(),
---
+++ lib/ripples_playlist/example4.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:riverpod_tutorials/main.dart';
+
+class Example4HomePage extends ConsumerStatefulWidget {
+  const Example4HomePage({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _Example4HomePageState();
+}
+
+class _Example4HomePageState extends ConsumerState<Example4HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    final count = ref.watch(countProvider);
+
+    // tp listen to changes
+    ref.listen(countProvider, ((previous, next) {
+      if ((next % 5) == 0) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text("The value changes to $next"),
+          ),
+        );
+      }
+    }));
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("State Provider Example"),
+        actions: [
+          IconButton(
+            onPressed: () {
+              // ref.invalidate(countProvider);
+              final newValue = ref.refresh(countProvider);
+            },
+            icon: const Icon(Icons.refresh),
+          ),
+        ],
+      ),
+      body: Center(
+        child: Text(
+          "Count value is $count",
+          style: const TextStyle(
+            fontSize: 30,
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // ref.read(countProvider.notifier).state++;
+          ref.read(countProvider.notifier).update((state) => state + 1);
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
